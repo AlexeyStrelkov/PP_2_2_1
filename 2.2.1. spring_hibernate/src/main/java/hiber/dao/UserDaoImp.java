98146cbfb1652dad2c78228089aa222fe64e9998
AlexeyStrelkov <saw98773@gmail.com>
2023-01-11 10:15:49 +0400

    edited according to comments

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,35 +1,37 @@
 package hiber.dao;
 
 import hiber.model.User;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-    @Autowired
-    private SessionFactory sessionFactory;
+    @PersistenceContext
+    private Session session;
+
+
 
     @Override
     public void add(User user) {
-        sessionFactory.getCurrentSession().save(user);
+        session.save(user);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        TypedQuery<User> query = session.createQuery("from User");
         return query.getResultList();
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public User userCar(String model, int series) {
-        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User where " +
+        TypedQuery<User> query = session.createQuery("from User where " +
                 "car.model = :model and car.series = :series");
         query.setParameter("model", model);
         query.setParameter("series", series);
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -11,8 +11,12 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-    @Autowired
     private UserDao userDao;
+
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Transactional
     @Override
@@ -26,7 +30,7 @@ public class UserServiceImp implements UserService {
         return userDao.listUsers();
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     @Override
     public User userCar(String model, int series) {
         return userDao.userCar(model, series);
